b3c585b2302937366dfe23843e294f91acf91d5e
Isaac Seymour <i.seymour@oxon.org>
2022-04-06 12:30:03 +0100

    Add bulk-permissions API
    The create-issue-meta API just returns a partial response if you don't
    have permission to create issues in that project. It's possible there
    are other reasons for that, but we can't really tell without the
    GetBulkPermissions API.

---
+++ permissions.go
@@ -0,0 +1,47 @@
+package jira
+
+import (
+	"context"
+)
+
+// PermissionsService handles projects for the Jira instance / API.
+//
+// Jira API docs: https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-permissions/
+type PermissionsService struct {
+	client *Client
+}
+
+// GetBulkPermissionsWithContext takes a list of projects with issue type IDs and
+// returns permissions given user (or currently-authed user if that is not set)
+// is granted on those projects and issue types. globalPermissions can also be
+// passed, which will check non-project-specific permissions too.
+//
+// https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-permissions/#api-rest-api-2-permissions-check-post
+func (s *PermissionsService) GetBulkPermissionsWithContext(ctx context.Context, accountID string, globalPermissions, permissions []string, projectIDs []int, issueTypeIDs []int) (*BulkPermissions, *Response, error) {
+	reqBody := requestBulkPermissions{
+		AccountID:         accountID,
+		GlobalPermissions: globalPermissions,
+		ProjectPermissions: []requestProjectPermission{{
+			Permissions:  permissions,
+			ProjectIDs:   projectIDs,
+			IssueTypeIDs: issueTypeIDs,
+		}},
+	}
+
+	req, err := s.client.NewRequestWithContext(ctx, "POST", "/rest/api/2/permissions/check", reqBody)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	bs := new(BulkPermissions)
+	resp, err := s.client.Do(req, bs)
+	if err != nil {
+		return nil, resp, NewJiraError(resp, err)
+	}
+
+	return bs, resp, nil
+}
+
+func (s *PermissionsService) GetBulkPermissions(accountID string, globalPermissions, permissions []string, projectIDs, issueTypeIDs []int) (*BulkPermissions, *Response, error) {
+	return s.GetBulkPermissionsWithContext(context.Background(), accountID, globalPermissions, permissions, projectIDs, issueTypeIDs)
+}
